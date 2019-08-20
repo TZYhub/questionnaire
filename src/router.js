fb2970328f9195812086fcc517a8dd8d8730999c
tanzy <542382610@qq.com>
2019-08-20 20:23:49 +0800

    引用bootstrapvue

--- src/router.js
+++ src/router.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from './views/Home.vue'
+import Login from './views/Login.vue'
+import Temp from './views/Temp.vue'
+import Register from './views/Register'
+import Share from './views/Share'
+import Statistics from './views/Statistics'
+import Home from './views/Home'
 
 Vue.use(Router)
 
@@ -8,16 +13,33 @@ export default new Router({
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: Login
+    },
+    {
+      path: '/home',
       name: 'home',
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/temp',
+      name: 'temp',
+      component: Temp
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: Register
+    },
+    {
+      path: '/share',
+      name: 'share',
+      component: Share
+    },
+    {
+      path: '/statistics',
+      name: 'statistics',
+      component: Statistics
     }
   ]
 })
