a3e1667e444938789da15463c19efe2b61b3b1f9
tanzy <542382610@qq.com>
2019-08-23 09:35:18 +0800

    优化页面

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,14 @@
 <template>
   <div>
-    <div class="col-10 offset-1 h">
-      <div class="h1">
-        <b-button to="/temp"> + 新增问卷</b-button>
-      </div>
-<!--      <div class="row mx-auto w-75 clear:both;">-->
-<!--        <div class="col-6">-->
-<!--          <div class="btn-group">-->
-<!--            <button type="button" class="btn btn-outline-info btn-sm" data-toggle="modal" data-target="#myModal">新增</button>-->
-<!--            <button type="button" class="btn btn-outline-primary btn-sm" @click="saveRows">保存</button>-->
-<!--          </div>-->
-<!--          <button type="button" class="btn btn-outline-warning btn-sm" @click="delRows">删除</button>-->
-<!--        </div>-->
-<!--        <div class="col-6">-->
-<!--          <div class="input-group">-->
-<!--            <input type="text" class="form-control input-group-sm" placeholder="输入设备编号进行搜索">-->
-<!--            <span class="input-group-btn">-->
-<!--                        <button class="btn btn-default" type="button"><i class="fa fa-search"></i></button>-->
-<!--                    </span>-->
-<!--          </div>-->
-<!--        </div>-->
-<!--      </div>-->
-      <div class="clear:both; ">
-        <div class="col-12">
-          <table class="table table-hover">
-            <thead class="thead-default">
-            <tr class="t2">
-              <th><input type="checkbox"></th>
+    <div class="col-10 offset-1">
+      <b-card>
+        <div class="card-header">
+          <b-button to="/temp"> + 新增问卷</b-button>
+        </div>
+        <b-card>
+          <table class="table table-striped">
+            <thead>
+            <tr>
               <th>标题</th>
               <th>类型</th>
               <th>收集状态</th>
@@ -39,7 +21,6 @@
             </thead>
             <tbody>
             <tr v-for="(facility,index) in facilities" :key="index" class="t3">
-              <td><input type="checkbox" :value="index" v-model="checkedRows"></td>
               <td>{{index+1}}</td>
               <td>{{facility.code}}</td>
               <td>{{facility.name}}</td>
@@ -51,8 +32,8 @@
             </tr>
             </tbody>
           </table>
-        </div>
-      </div>
+        </b-card>
+      </b-card>
     </div>
   </div>
 </template>
@@ -68,6 +49,7 @@ export default {
         { code: 3, name: 'test', states: 1, date: '2019-08-21', admin: 'tanzy' },
         { code: 4, name: 'test', states: 1, date: '2019-08-21', admin: 'tanzy' },
         { code: 4, name: 'test', states: 1, date: '2019-08-21', admin: 'tanzy' },
+        { code: 4, name: 'test', states: 1, date: '2019-08-21', admin: 'tanzy' },
         { code: 5, name: 'test', states: 1, date: '2019-08-21', admin: 'tanzy' }
       ]
     }
@@ -77,19 +59,4 @@ export default {
 
 <style scoped>
 
-.h{
-  margin-top: 1px;
-  min-height:500px;
-  background-color: lightgray;
-}
-.h1{
-  float:right;
-  padding-right: 15px;
-}
-.t2{
-  background-color: whitesmoke;
-}
-.t3{
-  background-color: floralwhite;
-}
 </style>
--- src/views/Temp.vue
+++ src/views/Temp.vue
@@ -1,123 +1,60 @@
 <template>
     <div>
-      <div class="col-6 offset-3 t1">
-        <div class="col-2 t2">
-          <ul>
-            <li>
-              <b-button class="t3" v-b-popover.hover="''" title="单选题" variant="outline-secondary">单选题</b-button>
-            </li>
-            <li>
-              <b-button class="t3" v-b-popover.hover="''" title="多选题" variant="outline-secondary">多选题</b-button>
-            </li>
-            <li>
-              <b-button class="t3" v-b-popover.hover="''" title="填空题" variant="outline-secondary">填空题</b-button>
-            </li>
-          </ul>
-        </div>
-        <div class="col-10 t2">
-          <div class="t4">
-            <p class="left">问卷标题</p>
-            <p class="left">描述</p>
-          </div>
-          <ul>
-            <li>
-              <div class="col-1 t5">
-                <p class="center">1</p>
+      <div class="col-10 offset-1">
+        <b-card class="col-2 left">
+          <b-list-group>
+            <b-list-group-item class="add-item-margin">单选题</b-list-group-item>
+            <b-list-group-item class="add-item-margin">多选题</b-list-group-item>
+            <b-list-group-item class="add-item-margin">填空题</b-list-group-item>
+          </b-list-group>
+        </b-card>
+        <b-card class="col-9 left">
+          <b-card>
+            <p>问卷标题</p>
+            <p class="text-left">提示话。。。。。。。。。。。。。</p>
+          </b-card>
+          <b-list-group>
+            <b-list-group-item class="d-flex justify-content-between align-items-center add-item-margin">
+              <div>
+                <p class="text-left">1    题目</p>
+                <p class="text-left">选项1</p>
+                <p class="text-left">选项2</p>
               </div>
-              <div class="col-11 t5">
-                <p class="left">题目描述</p>
-                <p class="left">选项1</p>
-                <p class="left">选项2</p>
+            </b-list-group-item>
+            <b-list-group-item class="d-flex justify-content-between align-items-center add-item-margin">
+              <div>
+              <p class="text-left">1    题目</p>
+              <p class="text-left">选项1</p>
+              <p class="text-left">选项2</p>
               </div>
-            </li>
-            <li>
-              <div class="col-1 t5">
-                <p class="center">2</p>
+            </b-list-group-item>
+            <b-list-group-item class="d-flex justify-content-between align-items-center add-item-margin">
+              <div>
+              <p class="text-left">1    题目</p>
+              <p class="text-left">选项1</p>
+              <p class="text-left">选项2</p>
               </div>
-              <div class="col-11 t5">
-                <p class="left">题目描述</p>
-                <p class="left">选项1</p>
-                <p class="left">选项2</p>
+            </b-list-group-item>
+            <b-list-group-item class="d-flex justify-content-between align-items-center add-item-margin">
+              <div>
+              <p class="text-left">1    题目</p>
+              <p class="text-left">选项1</p>
+              <p class="text-left">选项2</p>
               </div>
-            </li>
-          </ul>
-          <div class="t4">
-            <b-button>保存并发布</b-button>
-          </div>
-        </div>
+            </b-list-group-item>
+            <b-list-group-item class="d-flex justify-content-between align-items-center add-item-margin">
+              <div>
+              <p class="text-left">1    题目</p>
+              <p class="text-left">选项1</p>
+              <p class="text-left">选项2</p>
+              </div>
+            </b-list-group-item>
+
+          </b-list-group>
+        </b-card>
+        <div class="clear"></div>
       </div>
 
-
-
-
-
-
-
-
-
-
-      <!--<div class="text-left">-->
-        <!--<b-link to="/home">返回</b-link>-->
-      <!--</div>-->
-      <!--&lt;!&ndash;展示添加的列表&ndash;&gt;-->
-      <!--<div>-->
-          <!--<div v-for="(item,index) in questionList" :key="index">-->
-            <!--<b-input-group class="mb-2">-->
-              <!--<span>{{index+1}}</span>-->
-              <!--<b-form-input v-model="item.questionTitle"></b-form-input>-->
-            <!--</b-input-group>-->
-            <!--<div v-if="item.questionType === '1'">-->
-              <!--<div v-for="(questionItem,itemIndex) in item.questionItemList" :key="itemIndex">-->
-                <!--<b-input-group class="mb-2">-->
-                  <!--<b-input-group-prepend is-text>-->
-                    <!--<input type="radio" aria-label="Checkbox for following text input">-->
-                  <!--</b-input-group-prepend>-->
-                  <!--<b-form-input aria-label="Text input with checkbox" v-model="item.questionItemList[itemIndex]"></b-form-input>-->
-                <!--</b-input-group>-->
-              <!--</div>-->
-              <!--<button @click="addItem(index)">新增一个选项</button>-->
-            <!--</div>-->
-            <!--<div v-else-if="item.questionType === '2'">-->
-              <!--<div v-for="(questionItem,itemIndex) in item.questionItemList" :key="itemIndex">-->
-                <!--<b-input-group class="mb-2">-->
-                  <!--<b-input-group-prepend is-text>-->
-                    <!--<input type="checkbox" aria-label="Checkbox for following text input">-->
-                  <!--</b-input-group-prepend>-->
-                  <!--<b-form-input aria-label="Text input with checkbox" v-model="item.questionItemList[itemIndex]"></b-form-input>-->
-                <!--</b-input-group>-->
-              <!--</div>-->
-              <!--<button @click="addItem(index)">新增一个选项</button>-->
-            <!--</div>-->
-            <!--<div v-else-if="item.questionType === '3'">-->
-              <!--<b-form-textarea-->
-                <!--id="textarea"-->
-                <!--rows="3"-->
-                <!--max-rows="6"-->
-              <!--&gt;</b-form-textarea>-->
-            <!--</div>-->
-
-          <!--</div>-->
-      <!--</div>-->
-      <!--<div class="add-box">-->
-        <!--<p class="qu-type" v-if="isAdding">-->
-          <!--<button @click="addType('1')">单选题</button>-->
-          <!--<button @click="addType('2')">多选题</button>-->
-          <!--<button @click="addType('3')">文本题</button>-->
-        <!--</p>-->
-
-        <!--<b-button  variant="danger" @click="isAdding = !isAdding;">-->
-          <!--+ 添加问题-->
-        <!--</b-button>-->
-      <!--</div>-->
-      <!--<footer>-->
-        <!--<div>-->
-          <!--<label>问卷截止日期</label>-->
-        <!--</div>-->
-        <!--<div>-->
-          <!--<button @click="saveQuestionnaire()">保存问卷</button>-->
-          <!--<button>发布问卷</button>-->
-        <!--</div>-->
-      <!--</footer>-->
     </div>
 </template>
 
@@ -170,41 +107,15 @@ export default {
 </script>
 
 <style scoped>
-.t1{
-  border:1px solid red;
-  height: 500px;
-  padding: 0px;
-}
-.t2{
-  border:1px solid red;
-  padding: 0px;
-  float:left;
-  min-height: 400px;
-}
-.t3{
-  width: 100%;
-  margin-top: 3px;
-}
-.t4{
-  clear: both;
-  border:1px solid red;
-  width: 100%;
-  min-height: 100px;
-}
-.t5{
-  float:left;
-  border:1px solid red;
-}
-.center{
-  text-align:center;
-}
 .left{
-  text-align:left;
+  float: left;
+  height: 100%;
 }
-ul{
-  list-style-type: none;
-  padding: 0px;
-  margin: 0px;
+.clear{
+  clear: both;
+}
+.add-item-margin{
+  margin-top: 20px;
 }
 
 </style>
